89d225a41213b207852216b6a4e007f4211b308d
HsinJouChen <165478798+HsinJouChen@users.noreply.github.com>
2024-04-13 09:04:47 +0800

    Add files via upload

---
+++ Hello.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html>
+<head>
+<charset = "UTF-8">
+</head>
+<body>
+    <?php
+    $fruit[0] = "APPLE";
+    {
+
+    }
+
+    $fruit[] = "apple";
+    $fruit[] = "banana";
+    $fruit[] = "orange";
+
+    //echo "Hello World";
+    echo "<h1>Hello ", $fruit[0], "</h1>";
+
+    ?>
+</body>
+</html>
